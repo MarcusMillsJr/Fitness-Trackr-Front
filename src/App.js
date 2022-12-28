4772f2ef5dac07586d5a503b491e41e81afb81ce
Andy Nguyen <andyng1997@outlook.com>
2022-12-27 19:21:33 -0500

    organized it a bit, added all components

--- src/App.js
+++
@@ -1,4 +0,0 @@
-// import React, { useEffect, useState } from "react";
-// import { } from "./components";
-// import { Route, Switch, Link, useHistory } from "react-router-dom";
-// import { } from "./api/api";
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+//export {default as xxx} from './xxx;
+//do this for all components
