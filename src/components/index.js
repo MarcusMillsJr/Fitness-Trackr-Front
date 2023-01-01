332f3d68f09cae01e5844d37ca7464d396102d65
Marcus Mills Jr <marcus.mills97@yahoo.com>
2023-01-01 14:30:10 -0500

    commit and stash before andy's merge

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@
 export {default as Home} from "./Home"
 // export {default as Profile} from "./Profile"
 export {default as Login} from "./Login"
-export {default as Activities} from "./Activities"
+export {default as Activities} from "./Actvitites"
 export {default as Routines} from "./Routines"
 export {default as CreateActivity} from "./CreateActivity"
 // export {default as MyInbox} from "./MyInbox"
